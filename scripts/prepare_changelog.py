b86c3ee4c6ad09639878072c1d7a220b56a6d627
krajole <92697369+krajole@users.noreply.github.com>
2021-09-15 05:52:36 +0000

    READ ONLY: Daily import from FFMPEG git

--- scripts/prepare_changelog.py
+++ scripts/prepare_changelog.py
@@ -28,4 +28,5 @@ def main():
     lines.insert(
         insert_index + 1,
         f"## [{TAG}](https://github.com/epwalsh/rust-cached-path/releases/tag/{TAG}) - "
-        f"{datetime.now().
+        f"{datetime.now().strftime('%Y-%m-%d')}\n",
+ 
