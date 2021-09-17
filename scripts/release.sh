f8b149306b4c9cd888b2faa2bd9aafa7df724ae4
krajole <92697369+krajole@users.noreply.github.com>
2021-09-17 17:04:42 +0000

    Automatic changelog update

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-TAG=$(grep '^version =' Cargo.toml | head -n 1 | sed -E 's/version = "([^"]+)"/v\1/g'
+TAG=$(grep '^version =' Cargo.toml | head -n 1 | sed -E 's/version = "([^"]+)"/v\1/g')
+
+read -p "Creating new re
