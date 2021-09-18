8458b4d0fcfc60ca1e269fd022f09458801bd730
krajole <92697369+krajole@users.noreply.github.com>
2021-09-18 13:26:06 +0000

    Fix validation errors.

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,4 +4,4 @@ set -e
 
 TAG=$(grep '^version =' Cargo.toml | head -n 1 | sed -E 's/version = "([^"]+)"/v\1/g')
 
-read -p "Creating new release for $TAG. Do you wan
+read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
