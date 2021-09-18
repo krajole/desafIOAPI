4cd57a885ea7d62899a10384e1cd864e79e141d4
krajole <92697369+krajole@users.noreply.github.com>
2021-09-18 15:32:56 +0000

    renamed parameter

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,3 +5,5 @@ set -e
 TAG=$(grep '^version =' Cargo.toml | head -n 1 | sed -E 's/version = "([^"]+)"/v\1/g')
 
 read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
+
+if [[ $prompt == "y" || $prompt ==
