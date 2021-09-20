44424a7d6039cc4b415326e1ee2979b5f4b36c3b
krajole <92697369+krajole@users.noreply.github.com>
2021-09-20 07:45:23 +0000

    f2fs: fix to unlock f2fs_lock at the omitted error case

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,4 +6,5 @@ TAG=$(grep '^version =' Cargo.toml | head -n 1 | sed -E 's/version = "([^"]+)"/v
 
 read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
 
-if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]];
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+    TAG=$TAG python scrip
