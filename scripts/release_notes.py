159c1d4db21e127a592cec6bbb95c3dc4f1d4654
krajole <92697369+krajole@users.noreply.github.com>
2021-10-07 11:09:07 +0000

    add default value

--- scripts/release_notes.py
+++ scripts/release_notes.py
@@ -32,4 +32,4 @@ def get_change_log_notes() -> str:
             if in_current_section:
                 if line.startswith("### Added"):
                     line = ADDED_HEADER + "\n"
-                el
+                elif line.startswith
