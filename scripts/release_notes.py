e42467911c0384ba877b264b97fab4b5b01f284c
krajole <92697369+krajole@users.noreply.github.com>
2021-10-12 06:58:55 +0000

    Added test names.

--- scripts/release_notes.py
+++ scripts/release_notes.py
@@ -40,4 +40,7 @@ def get_change_log_notes() -> str:
                     line = REMOVED_HEADER + "\n"
                 current_section_notes.append(line)
     assert current_section_notes
-    return "## What's new\n\n" + "".join(current_sectio
+    return "## What's new\n\n" + "".join(current_section_notes).strip() + "\n"
+
+
+def
