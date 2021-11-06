95243ccc42cd34e2de4add0b9f5558050420f39a
krajole <92697369+krajole@users.noreply.github.com>
2021-11-06 17:30:05 +0000

    Fix some mistakes

--- src/archives.rs
+++ src/archives.rs
@@ -15,4 +15,4 @@ impl ArchiveFormat {
     pub(crate) fn parse_from_extension(resource: &str) -> Result<Self, Error> {
         if resource.ends_with(".tar.gz") {
             Ok(Self::TarGz)
-        } el
+        } else if resource.ends_with(".zip
