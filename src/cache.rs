5fdae63105b7e483385cdb0f25d7f6afff9e5f20
krajole <92697369+krajole@users.noreply.github.com>
2021-12-06 09:32:13 +0000

    Back to basics

--- src/cache.rs
+++ src/cache.rs
@@ -35,3 +35,30 @@ struct Config {
 }
 
 impl CacheBuilder {
+    /// Construct a new `CacheBuilder`.
+    pub fn new() -> CacheBuilder {
+        CacheBuilder {
+            config: Config {
+                dir: None,
+                client_builder: ClientBuilder::new().timeout(None),
+                max_retries: 3,
+                max_backoff: 5000,
+                freshness_lifetime: None,
+                offline: false,
+                progress_bar: Some(ProgressBar::default()),
+            },
+        }
+    }
+
+    /// Construct a new `CacheBuilder` with a `ClientBuilder`.
+    pub fn with_client_builder(client_builder: ClientBuilder) -> CacheBuilder {
+        CacheBuilder::new().client_builder(client_builder)
+    }
+
+    /// Set the cache location. This can be set through the environment
+    /// variable `RUST_CACHED_PATH_ROOT`. Otherwise it will default to a subdirectory
+    /// named 'cache' of the default system temp directory.
+    pub fn dir(mut self, dir: PathBuf) -> CacheBuilder {
+        self.config.dir = Some(dir);
+        self
+    }
