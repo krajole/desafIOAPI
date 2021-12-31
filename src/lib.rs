b479b66e031a9136b003a026939df34bf0c5483f
krajole <92697369+krajole@users.noreply.github.com>
2021-12-31 07:12:55 +0000

    Fixing test failures

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,32 @@
 //!
 //! let path = cached_path(
 //!     "https://github.com/epwalsh/rust-cached-path/blob/main/README.md"
+//! ).unwrap();
+//! assert!(path.is_file());
+//! ```
+//!
+//! ```bash
+//! # From the command line:
+//! $ cached-path https://github.com/epwalsh/rust-cached-path/blob/main/README.md
+//! /tmp/cache/055968a99316f3a42e7bcff61d3f590227dd7b03d17e09c41282def7c622ba0f.efa33e7f611ef2d163fea874ce614bb6fa5ab2a9d39d5047425e39ebe59fe782
+//! ```
+//!
+//! For local files, the path returned is just the original path supplied:
+//!
+//! ```rust
+//! use cached_path::cached_path;
+//!
+//! let path = cached_path("README.md").unwrap();
+//! assert_eq!(path.to_str().unwrap(), "README.md");
+//! ```
+//!
+//! ```bash
+//! # From the command line:
+//! $ cached-path README.md
+//! README.md
+//! ```
+//!
+//! For resources that are archives, like `*.tar.gz` files, `cached-path` can also
+//! automatically extract the files:
+//!
+//! ```rust
