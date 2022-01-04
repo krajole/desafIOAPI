049983236367e735c0e1df5e3eaa3b3f819ddd3f
krajole <92697369+krajole@users.noreply.github.com>
2022-01-04 22:24:24 +0000

    Updated Doom (markdown)

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,37 @@ use structopt::StructOpt;
 #[structopt(
     name = "cached-path",
     about = "Get the cached path to a resource.",
+    setting = structopt::clap::AppSettings::ColoredHelp,
+)]
+struct Opt {
+    #[structopt()]
+    /// The resource path.
+    resource: String,
+
+    #[structopt(long = "dir", env = "RUST_CACHED_PATH_ROOT")]
+    /// The cache directory. Defaults to a subdirectory named 'cache' of the default
+    /// system temporary directory.
+    dir: Option<PathBuf>,
+
+    #[structopt(long = "subdir")]
+    /// The subdirectory, relative to the cache root directory to use.
+    subdir: Option<String>,
+
+    #[structopt(long = "extract")]
+    /// Extract the resource as an archive.
+    extract: bool,
+
+    #[structopt(long = "timeout")]
+    /// Set a request timeout.
+    timeout: Option<u64>,
+
+    #[structopt(long = "connect-timeout")]
+    /// Set a timeout for the connect phase of the HTTP client.
+    connect_timeout: Option<u64>,
+
+    #[structopt(long = "max-retries", default_value = "3")]
+    /// Set the maximum number of times to retry an HTTP request. Retriable failures are tried
+    /// again with exponential backoff.
+    max_retries: u32,
+
+    #[structopt(long = "max-backoff", default_value = "5000")]
