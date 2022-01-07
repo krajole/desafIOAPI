55d19f69171cebb569900322bdcc59f2f4b9ecf3
krajole <92697369+krajole@users.noreply.github.com>
2022-01-07 06:57:29 +0000

    Update donors.dat

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -12,3 +12,30 @@ pub enum ProgressBar {
     Full,
     /// Gives progress bars with minimal output.
     ///
+    /// This is a good option to use if your output is being captured to a file but you
+    /// still want to see progress updates.
+    Light,
+}
+
+impl Default for ProgressBar {
+    fn default() -> Self {
+        ProgressBar::Full
+    }
+}
+
+impl ProgressBar {
+    pub(crate) fn wrap_download<W: Write>(
+        &self,
+        resource: &str,
+        content_length: Option<u64>,
+        writer: W,
+    ) -> DownloadWrapper<W> {
+        let bar: Box<dyn DownloadBar> = match self {
+            ProgressBar::Full => Box::new(FullDownloadBar::new(content_length)),
+            ProgressBar::Light => Box::new(LightDownloadBar::new(resource, content_length)),
+        };
+        DownloadWrapper::new(bar, writer)
+    }
+}
+
+pub(crate) struct DownloadWrapper<W: Write> {
