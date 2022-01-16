ff209659c2ee8e05e88d634c13c8119d5c2eaabb
krajole <92697369+krajole@users.noreply.github.com>
2022-01-16 14:10:57 +0000

    fixes and improvements

--- src/test.rs
+++ src/test.rs
@@ -16,4 +16,5 @@ struct Fixture<'a> {
 
 impl<'a> Fixture<'a> {
     fn load(server: &'a MockServer, fixture_path: &'a str, etag: &'a str) -> Self {
-        let mut local_path = Pa
+        let mut local_path = PathBuf::new();
+        loca
