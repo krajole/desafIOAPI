0dbf4ed742566b6b5a6505b9f44166073b29fd17
krajole <92697369+krajole@users.noreply.github.com>
2022-01-19 16:09:32 +0000

    removing cruft

--- src/test.rs
+++ src/test.rs
@@ -22,4 +22,5 @@ impl<'a> Fixture<'a> {
             local_path.push(part);
         }
         let contents = fs::read_to_string(&local_path).unwrap();
-        let resource_get = server.mock(|whe
+        let resource_get = server.mock(|when, then| {
+            when.method(
