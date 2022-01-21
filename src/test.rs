c2b20673720b1c44b67e246369172f9497589d95
krajole <92697369+krajole@users.noreply.github.com>
2022-01-21 18:59:46 +0000

    add anchor

--- src/test.rs
+++ src/test.rs
@@ -27,4 +27,4 @@ impl<'a> Fixture<'a> {
             then.status(200).header(ETAG_KEY, etag).body(&contents);
         });
         let resource_head = server.mock(|when, then| {
-           
+            when.method(HEAD).path(fo
