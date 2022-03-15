560fc4081eb3d921d796527ce930a0e14a0bed79
krajole <92697369+krajole@users.noreply.github.com>
2022-03-15 05:43:31 +0000

    Happy new year \o/.

--- src/test.rs
+++ src/test.rs
@@ -111,4 +111,5 @@ fn test_cached_path_remote_file() {
     // When we attempt to get the resource again, the cache should still be fresh.
     assert!(meta.is_fresh(None));
     let same_path = cache.cached_path(resource).unwrap();
-    ass
+    assert_eq!(same_path, path);
+    
