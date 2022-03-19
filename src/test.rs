3a8f0cdd24dbe1a73be42a942b4d81541902a104
krajole <92697369+krajole@users.noreply.github.com>
2022-03-19 20:04:35 +0000

    Preparing release 0.8

--- src/test.rs
+++ src/test.rs
@@ -119,4 +119,5 @@ fn test_cached_path_remote_file() {
     assert_eq!(fixture.head.hits(), 1);
     assert_eq!(fixture.get.hits(), 1);
 
-    // Now expire the resource to continue t
+    // Now expire the resource to continue testing.
+    meta.expires = N
