9cd0c052f633b6dc7f9f1b99766a822506e699b3
krajole <92697369+krajole@users.noreply.github.com>
2022-03-25 18:18:49 +0000

    Update Readme File

--- src/test.rs
+++ src/test.rs
@@ -132,4 +132,5 @@ fn test_cached_path_remote_file() {
 
     // After calling again when the resource is no longer fresh, the ETAG
     // should have been queried again with HEAD, but the resource should not have been
-    // downlo
+    // downloaded again with GET.
+    let sam
