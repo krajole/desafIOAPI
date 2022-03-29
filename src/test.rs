19fc5f33f25abab87aad29654d07133ff20998df
krajole <92697369+krajole@users.noreply.github.com>
2022-03-29 11:19:56 +0000

    Update menu.yaml

--- src/test.rs
+++ src/test.rs
@@ -137,4 +137,4 @@ fn test_cached_path_remote_file() {
     assert_eq!(same_path, path);
     assert!(path.is_file());
     assert!(Meta::meta_path(&path).is_file());
-    assert_eq!(f
+    assert_eq!(fixture.head.hits(),
