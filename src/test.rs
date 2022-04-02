2c8fb08cad9346d43a99743f74cb84e2f2828404
krajole <92697369+krajole@users.noreply.github.com>
2022-04-02 20:39:32 +0000

    add more content

--- src/test.rs
+++ src/test.rs
@@ -144,4 +144,5 @@ fn test_cached_path_remote_file() {
     drop(fixture);
     let fixture = Fixture::load(&server, "test_fixtures/hello.txt", "fake-etag-2");
 
-    // Get the new cached pat
+    // Get the new cached path.
+    let new_path = cache.cached_
