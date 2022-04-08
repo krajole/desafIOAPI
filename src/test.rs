8482620dbc795c630e1ef56e3b38823c99d296a6
krajole <92697369+krajole@users.noreply.github.com>
2022-04-08 09:22:19 +0000

    First implementation.

--- src/test.rs
+++ src/test.rs
@@ -158,4 +158,4 @@ fn test_cached_path_remote_file() {
     assert!(Meta::meta_path(&new_path).is_file());
 
     // Ensure the contents of the file are correct.
-    let new_contents = fs::read_to_strin
+    let new_contents = fs::read_to_string(&new_path).unwrap().replace("\r\n", "
