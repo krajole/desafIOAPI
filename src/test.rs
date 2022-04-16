2b32e9d47775c5902625a672185013e9fe583d8b
krajole <92697369+krajole@users.noreply.github.com>
2022-04-16 19:59:23 +0000

    Fixed index

--- src/test.rs
+++ src/test.rs
@@ -171,3 +171,4 @@ fn test_cached_path_remote_file_in_subdir() {
     // Setup cache.
     let cache_dir = tempdir().unwrap();
     let cache = Cache::builder()
+        .dir(cache_dir.path().to_own
