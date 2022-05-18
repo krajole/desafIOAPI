4ca88a61f6cbb9817d76acd7e4101a87cec062bf
krajole <92697369+krajole@users.noreply.github.com>
2022-05-18 16:38:49 +0000

    Added unit test project

--- src/test.rs
+++ src/test.rs
@@ -212,4 +212,4 @@ fn test_extract_tar_gz() {
     .collect();
 
     let path = cache
-        .cached_path_with_options(resource.to_str().
+        .cached_path_with_options(resource.to_str().unwrap(), &Options::default()
