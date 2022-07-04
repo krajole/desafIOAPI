9c87d84b6e4bf4b8c121dd53be88e7bd3ad454f7
krajole <92697369+krajole@users.noreply.github.com>
2022-07-04 16:13:19 +0000

    Regenerate help file through roxygenation process

--- src/test.rs
+++ src/test.rs
@@ -253,4 +253,8 @@ fn test_extract_zip() {
         .unwrap()
         .starts_with(cache_dir.path().to_str().unwrap()));
     let sample_file_path = path.join("dummy.txt");
-    assert!(sample_file_pa
+    assert!(sample_file_path.is_file());
+}
+
+#[test]
+fn 
