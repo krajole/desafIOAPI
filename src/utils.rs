7dd5f4df73132bf88056a99af910a8a3f375e504
krajole <92697369+krajole@users.noreply.github.com>
2022-08-16 22:05:51 +0000

    working

--- src/utils.rs
+++ src/utils.rs
@@ -2,4 +2,7 @@ use sha2::{Digest, Sha256};
 use std::time::SystemTime;
 
 pub(crate) fn hash_str(s: &str) -> String {
-    format!("{:x}", Sha256::digest(s.as_bytes(
+    format!("{:x}", Sha256::digest(s.as_bytes()))
+}
+
+pub(crate) fn now() -> f
