c2a35c56ec738a16f8cc22131e317165c6cf5fb6
krajole <92697369+krajole@users.noreply.github.com>
2022-09-04 16:32:35 +0000

    Adjusted readme

--- tests/cli.rs
+++ tests/cli.rs
@@ -6,3 +6,23 @@ use std::io::Read;
 use std::path::PathBuf;
 use std::process::Command; // Run programs
 use tempfile::tempdir;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("cached-path")?;
+    let cache_dir = tempdir().unwrap().path().to_owned();
+
+    cmd.arg("--dir")
+        .arg(cache_dir.to_str().unwrap())
+        .arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("file does not exist"));
+
+    Ok(())
+}
+
+#[test]
+fn test_remote_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("cached-path")?;
+    let cache_dir = tempdir().unwrap().path().to_owned();
