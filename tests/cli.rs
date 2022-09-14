c4f480524fc1afc6c4b0b248178be3177613c81a
krajole <92697369+krajole@users.noreply.github.com>
2022-09-14 15:18:30 +0000

    Updates for Griffon 0.9.5

--- tests/cli.rs
+++ tests/cli.rs
@@ -26,3 +26,31 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 fn test_remote_file() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("cached-path")?;
     let cache_dir = tempdir().unwrap().path().to_owned();
+
+    cmd.arg("--dir")
+        .arg(cache_dir.to_str().unwrap())
+        .arg("https://raw.githubusercontent.com/epwalsh/rust-cached-path/main/test_fixtures/utf-8_sample/utf-8_sample.txt");
+    let result = cmd.assert().success();
+    let output = result.get_output();
+    let mut stdout = String::from_utf8(output.stdout.clone()).unwrap();
+    // remove newline at the end.
+    stdout.pop();
+    let path = PathBuf::from(stdout);
+    println!("{:?}", path);
+    assert!(path.is_file());
+
+    // Ensure cached version exactly matches local version.
+    let mut cached_file = fs::File::open(&path)?;
+    let mut cached_contents = String::new();
+    cached_file.read_to_string(&mut cached_contents)?;
+
+    let local_path: PathBuf = [".", "test_fixtures", "utf-8_sample", "utf-8_sample.txt"]
+        .iter()
+        .collect();
+    assert!(local_path.is_file());
+    let mut local_file = fs::File::open(local_path)?;
+    let mut local_contents = String::new();
+    local_file.read_to_string(&mut local_contents)?;
+
+    // On Windows, git will automatically convert '\n' line-endings to '\r\n'.
+    // So we change those back.
