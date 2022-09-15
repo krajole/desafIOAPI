7c84694d5646947ff4f6023b2cdf47446d1774c5
krajole <92697369+krajole@users.noreply.github.com>
2022-09-15 10:04:57 +0000

    rm dead code

--- tests/cli.rs
+++ tests/cli.rs
@@ -54,3 +54,34 @@ fn test_remote_file() -> Result<(), Box<dyn std::error::Error>> {
 
     // On Windows, git will automatically convert '\n' line-endings to '\r\n'.
     // So we change those back.
+    let local_contents = local_contents.replace("\r\n", "\n");
+
+    assert_eq!(local_contents, cached_contents);
+
+    Ok(())
+}
+
+#[test]
+fn test_extract_remote_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("cached-path")?;
+    let cache_dir = tempdir().unwrap().path().to_owned();
+
+    cmd.arg("--dir")
+        .arg(cache_dir.to_str().unwrap())
+        .arg("--extract").arg(
+            "https://raw.githubusercontent.com/epwalsh/rust-cached-path/main/test_fixtures/utf-8_sample/archives/utf-8.tar.gz"
+        );
+    let result = cmd.assert().success();
+    let output = result.get_output();
+    let mut stdout = String::from_utf8(output.stdout.clone()).unwrap();
+    // remove newline at the end.
+    stdout.pop();
+    let path = PathBuf::from(stdout);
+    println!("{:?}", path);
+    assert!(path.is_dir());
+    assert!(path.join("dummy.txt").is_file());
+    assert!(path.join("folder").is_dir());
+    assert!(path.join("folder").join("utf-8_sample.txt").is_file());
+
+    Ok(())
+}
